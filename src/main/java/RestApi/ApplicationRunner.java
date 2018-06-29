b3a087fc4003384e35cc96afc5dc6dab838bad9c
Pravin Kumar <prkumartech@gmail.com>
2018-06-28 21:59:17 -0500

    added get and post method

--- src/main/java/RestApi/ApplicationRunner.java
+++ src/main/java/RestApi/ApplicationRunner.java
@@ -3,9 +3,24 @@ package RestApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class ApplicationRunner {
     public static void main(String[] args){
         SpringApplication.run(ApplicationRunner.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner init(CustomerRepository customerRepository) {
+//        return (evt) -> Arrays.asList(
+//                "jhoeller,dsyer,pwebb,ogierke,rwinch,mfisher,mpollack,jlong".split(","))
+//                .forEach(
+//                        a -> {
+//                            Customer customer = customerRepository.save(new Customer(a,"a2"));
+//                            customerRepository.save(new Customer(a,"b2"));
+//                            customerRepository.save(new Customer(a,"c2"));
+//                        });
+//    }
+
 }
--- src/main/java/RestApi/CustomerController.java
+++ src/main/java/RestApi/CustomerController.java
@@ -1,14 +1,33 @@
 package RestApi;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import model.Customer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @RestController
+@RequestMapping("/{accountNumber}/customer")
 public class CustomerController {
 
-    @RequestMapping(value = "/", method = GET)
-    public String index(){
-        return "congratulation from customer controller";
+    @GetMapping
+    Collection<Customer> getCustomer(@PathVariable String accountNumber) {
+        Collection<Customer> customerCollection = new ArrayList<>();
+        Customer customer = new Customer();
+        customer.setFirstName("John");
+        customer.setLastName("Smith");
+        customer.setAccountNumber(accountNumber);
+        customerCollection.add(customer);
+        return customerCollection;
     }
+
+    @PostMapping
+    public @ResponseBody ResponseEntity<String> post(@RequestBody Customer customer) {
+        return new ResponseEntity<String>("POST Response" + customer, HttpStatus.OK);
+    }
+
 }
